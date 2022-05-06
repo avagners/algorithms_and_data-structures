6e76b0e6f481c4819c52166bee234833d6fb2003
Alexander Vagner <onemybox@gmail.com>
2022-05-06 11:11:08 +0300

    added def partition(array, pivot)

--- algorithms/quicksort.py
+++ algorithms/quicksort.py
@@ -1,14 +1,21 @@
-from typing import List
+from typing import List, Tuple
+from random import choice
+
+
+def partition(array, pivot) -> Tuple[List[int], List[int], List[int]]:
+    less = [i for i in array if i < pivot]
+    center = [i for i in array if i == pivot]
+    greater = [i for i in array if i > pivot]
+    return less, center, greater
 
 
 def quicksort(array: list[int]) -> List[int]:
     if len(array) < 2:
         return array
     else:
-        pivot = array[0]
-        less = [i for i in array if i < pivot]
-        greater = [i for i in array if i > pivot]
-        return quicksort(less) + [pivot] + quicksort(greater)
+        pivot = choice(array)
+        less, center, greater = partition(array, pivot)
+        return quicksort(less) + center + quicksort(greater)
 
 
 def read_input() -> List[int]:
