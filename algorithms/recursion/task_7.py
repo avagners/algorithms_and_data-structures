1260dd1a68dcba6b8ce608086beefb5cbc0b1de5
Vagner Alexander <onemybox@gmail.com>
2022-10-26 16:19:14 +0300

    добавил аннотацию типов

--- algorithms/recursion/task_7.py
+++ algorithms/recursion/task_7.py
@@ -1,4 +1,4 @@
-def find_second_max(n, max_value, second_value):
+def find_second_max(n: list, max_value: int, second_value: int) -> tuple:
     current = n.pop()
     if current > max_value:
         second_value = max_value
@@ -10,7 +10,7 @@ def find_second_max(n, max_value, second_value):
     return max_value, second_value
 
 
-def main(n):
+def main(n: list) -> int:
     number_1, number_2 = n[0], n[1]
     max_value, second_value = ((number_1, number_2) if number_1 >= number_2
                                else (number_2, number_1))
