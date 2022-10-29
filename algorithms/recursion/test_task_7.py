280545ecf4a4e8c21cd539490dafab4c3145629b
Vagner Alexander <onemybox@gmail.com>
2022-10-29 11:11:12 +0300

    возвращает второе максимальное значение

--- algorithms/recursion/test_task_7.py
+++ algorithms/recursion/test_task_7.py
@@ -1,28 +1,28 @@
 import unittest
 
-from task_7 import find_second_max_value
+from task_7 import main
 
 
 class Tests(unittest.TestCase):
 
     def test_1(self):
         array = [0, 1, 2, 3, 4, 5, 6, 7]
-        result = find_second_max_value(array)
+        result = main(array)
         self.assertEqual(result, 6)
 
     def test_2(self):
         array = [0, 1, 2, 3, 4, 5, 6, 7, 7, 7]
-        result = find_second_max_value(array)
+        result = main(array)
         self.assertEqual(result, 7)
 
     def test_3(self):
         array = [6, 6, 7]
-        result = find_second_max_value(array)
+        result = main(array)
         self.assertEqual(result, 6)
 
     def test_4(self):
         array = [5, 4, 3, 2, 5]
-        result = find_second_max_value(array)
+        result = main(array)
         self.assertEqual(result, 5)
 
 
