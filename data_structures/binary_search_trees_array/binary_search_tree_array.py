106bd45c157d63425384041dc41fd00f3b7bb223
Vagner Alexander <onemybox@gmail.com>
2022-11-10 00:11:45 +0300

    метод поиска индекса по ключу узла

--- data_structures/binary_search_trees_array/binary_search_tree_array.py
+++ data_structures/binary_search_trees_array/binary_search_tree_array.py
@@ -5,9 +5,23 @@ class aBST:
         tree_size = 2**(depth + 1) - 1
         self.Tree = [None] * tree_size  # массив ключей
 
+    def __find_index(self, key, index):
+        if index > len(self.Tree):
+            return None
+        if not self.Tree[index]:
+            return -index
+        if self.Tree[index] == key:
+            return index
+        if self.Tree[index] > key:
+            index_left_child = 2 * index + 1
+            return self.__find_index(key, index_left_child)
+        if self.Tree[index] < key:
+            index_rignt_child = 2 * index + 2
+            return self.__find_index(key, index_rignt_child)
+
     def FindKeyIndex(self, key):
         # ищем в массиве индекс ключа
-        return None  # не найден
+        return self.__find_index(key, 0)
 
     def AddKey(self, key):
         # добавляем ключ в массив
--- data_structures/binary_search_trees_array/tests.py
+++ data_structures/binary_search_trees_array/tests.py
@@ -3,13 +3,39 @@ import unittest
 from binary_search_tree_array import aBST
 
 
-class TestBSTree(unittest.TestCase):
+class TestABSTreeSize(unittest.TestCase):
 
-    def setUp(self):
+    def test_tree_size(self):
         self.tree = aBST(2)
+        self.assertEqual(len(self.tree.Tree), 7)
+        self.tree2 = aBST(3)
+        self.assertEqual(len(self.tree2.Tree), 15)
+        self.tree3 = aBST(4)
+        self.assertEqual(len(self.tree3.Tree), 31)
 
-    def test(self):
-        print(self.tree.Tree)
+
+class TestABST(unittest.TestCase):
+
+    def test_full_tree(self):
+        tree = aBST(2)
+        tree.Tree = [50, 25, 75, 15, 35, 65, 100]
+        for index, key in enumerate(tree.Tree):
+            self.assertEqual(tree.FindKeyIndex(key), index)
+
+    def test_one_none_value(self):
+        tree = aBST(2)
+        tree.Tree = [50, 25, 75, 15, 35, 65, None]
+        self.assertEqual(tree.FindKeyIndex(90), -6)
+
+    def test_if_not_exists(self):
+        tree = aBST(2)
+        tree.Tree = [50, 25, 75, 15, 35, 65, 100]
+        self.assertIsNone(tree.FindKeyIndex(1000))
+
+    def test_empty_tree(self):
+        tree = aBST(1)
+        tree.Tree = [None, None, None]
+        self.assertEqual(tree.FindKeyIndex(1000), 0)
 
 
 if __name__ == '__main__':
