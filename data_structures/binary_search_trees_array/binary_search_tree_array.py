d49a8c40e6817623605ddbe018093acd07ed592e
Vagner Alexander <onemybox@gmail.com>
2022-11-10 10:13:44 +0300

    добавил метод добавления ключа в массив

--- data_structures/binary_search_trees_array/binary_search_tree_array.py
+++ data_structures/binary_search_trees_array/binary_search_tree_array.py
@@ -1,12 +1,11 @@
 class aBST:
 
     def __init__(self, depth: int):
-        # правильно рассчитайте размер массива для дерева глубины depth:
         tree_size: int = 2**(depth + 1) - 1
         self.Tree: list = [None] * tree_size  # массив ключей
 
     def __find_index(self, key: int, index: int) -> int:
-        if index > len(self.Tree):
+        if index >= len(self.Tree):
             return None
         if not self.Tree[index]:
             return -index
@@ -20,10 +19,19 @@ class aBST:
             return self.__find_index(key, index_rignt_child)
 
     def FindKeyIndex(self, key: int) -> int:
-        # ищем в массиве индекс ключа
         return self.__find_index(key, 0)
 
     def AddKey(self, key: int) -> int:
-        # добавляем ключ в массив
-        return -1
-        # индекс добавленного/существующего ключа или -1 если не удалось
+        """
+        Метод возвращает индекс добавленного/существующего ключа
+        или -1 если не удалось.
+        """
+        index = self.FindKeyIndex(key)
+        if index is None:
+            return -1
+        if index < 0 or not self.Tree[0]:
+            index *= -1
+            self.Tree[index] = key
+            return index
+        if index >= 0:
+            return index
--- data_structures/binary_search_trees_array/tests.py
+++ data_structures/binary_search_trees_array/tests.py
@@ -34,8 +34,42 @@ class TestABST(unittest.TestCase):
 
     def test_empty_tree(self):
         tree = aBST(1)
-        tree.Tree = [None, None, None]
         self.assertEqual(tree.FindKeyIndex(1000), 0)
+
+    def test_add_key(self):
+        tree = aBST(2)
+        tree.Tree = [50, 25, 75, 15, 35, 65, None]
+        self.assertEqual(tree.AddKey(90), 6)
+        self.assertEqual(tree.Tree[6], 90)
+
+    def test_add_key_in_empty_tree(self):
+        tree = aBST(1)
+        self.assertEqual(tree.AddKey(90), 0)
+        self.assertEqual(tree.Tree[0], 90)
+
+    def test_add_existing_key(self):
+        tree = aBST(2)
+        tree.Tree = [50, 25, 75, 15, 35, 65, 90]
+        self.assertEqual(tree.AddKey(50), 0)
+        self.assertEqual(tree.Tree[0], 50)
+        self.assertEqual(tree.AddKey(90), 6)
+        self.assertEqual(tree.Tree[6], 90)
+        self.assertEqual(tree.AddKey(15), 3)
+        self.assertEqual(tree.Tree[3], 15)
+
+    def test_add_key_in_full_tree(self):
+        tree = aBST(2)
+        tree.Tree = [50, 25, 75, 15, 35, 65, 90]
+        self.assertEqual(tree.AddKey(100), -1)
+        self.assertNotIn(100, tree.Tree)
+
+    def test_add_key_if_no_suitable_place(self):
+        tree = aBST(2)
+        tree.Tree = [50, 25, 75, 15, 35, 65, None]
+        self.assertEqual(tree.AddKey(60), -1)
+        self.assertNotIn(60, tree.Tree)
+        self.assertEqual(tree.AddKey(1), -1)
+        self.assertNotIn(1, tree.Tree)
 
 
 if __name__ == '__main__':
