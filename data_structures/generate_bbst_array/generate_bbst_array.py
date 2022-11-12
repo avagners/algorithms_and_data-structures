c2f90a81e8e5d27575342ff46bdce29a46457e11
Vagner Alexander <onemybox@gmail.com>
2022-11-12 22:19:59 +0300

    функция преобразования неупорядоченного массива в сбалансированное двоичное дерево поиска

--- data_structures/generate_bbst_array/generate_bbst_array.py
+++ data_structures/generate_bbst_array/generate_bbst_array.py
@@ -1,2 +1,21 @@
 def GenerateBBSTArray(a: list) -> list:
-    return None
+
+    def array_to_abbst(sorted_array, bbst_array, index):
+        middle_index = len(sorted_array) // 2
+        bbst_array[index] = sorted_array[middle_index]
+        left_subtree = sorted_array[:middle_index]
+        if len(left_subtree) > 0:
+            left_child_index = 2 * index + 1
+            array_to_abbst(left_subtree, bbst_array, left_child_index)
+        right_subtree = sorted_array[middle_index+1:]
+        if len(right_subtree) > 0:
+            right_child_index = 2 * index + 2
+            array_to_abbst(right_subtree, bbst_array, right_child_index)
+        return bbst_array
+
+    if not a:
+        return []
+    sorted_array = sorted(a)
+    bbst_array = [None] * len(a)
+    root_index = 0
+    return array_to_abbst(sorted_array, bbst_array, root_index)
--- data_structures/generate_bbst_array/tests.py
+++ data_structures/generate_bbst_array/tests.py
@@ -4,7 +4,39 @@ from generate_bbst_array import GenerateBBSTArray
 
 
 class TestGenerateBBSTArray(unittest.TestCase):
-    pass
+
+    def test_empty_array(self):
+        a = []
+        self.assertFalse(GenerateBBSTArray(a))
+        self.assertIsInstance(GenerateBBSTArray(a), list)
+        self.assertEqual(GenerateBBSTArray(a), [])
+
+    def test_one_level(self):
+        a = [1]
+        self.assertTrue(GenerateBBSTArray(a))
+        self.assertIsInstance(GenerateBBSTArray(a), list)
+        self.assertEqual(GenerateBBSTArray(a), [1])
+
+    def test_two_levels(self):
+        a = [1, 2, 3]
+        self.assertTrue(GenerateBBSTArray(a))
+        self.assertIsInstance(GenerateBBSTArray(a), list)
+        self.assertEqual(GenerateBBSTArray(a), [2, 1, 3])
+
+    def test_three_levels(self):
+        a = [1, 2, 3, 4, 5, 6, 7]
+        self.assertTrue(GenerateBBSTArray(a))
+        self.assertIsInstance(GenerateBBSTArray(a), list)
+        self.assertEqual(GenerateBBSTArray(a), [4, 2, 6, 1, 3, 5, 7])
+
+    def test_four_levels(self):
+        a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        self.assertTrue(GenerateBBSTArray(a))
+        self.assertIsInstance(GenerateBBSTArray(a), list)
+        self.assertEqual(
+            GenerateBBSTArray(a),
+            [8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15]
+        )
 
 
 if __name__ == '__main__':
